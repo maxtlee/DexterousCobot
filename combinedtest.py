8bff489e222da6a0e5f7620ee7fa44d258e4a55d
maxtlee <maxwell.lee.372007@gmail.com>
2026-06-10 13:15:28 -0400

    basic image filter works but needs tuning

--- combinedtest.py
+++ combinedtest.py
@@ -19,36 +19,12 @@ armB = (95*3.14/180, 16*3.14/180, 144*3.14/180, 22*3.14/180, -270*3.14/180, 180*
 handOpen = [40, 40, 40, 40, 20, -60]
 handClosed = [70, 70, 70, 70, 50, -80]
 
-def main():
-    arm = StandardBotsRobot(
-        url='http://192.168.1.3:3000', 
-        token=Path("/home/max/Documents/.robot_token").read_text().strip(),
-        robot_kind=StandardBotsRobot.RobotKind.Live,
-    )
+groundColor = ([75,70,68], [20,15,15])
+tolerance = 2
+# ballColor = ([193,192,127], [33*tolerance,38*tolerance,20*tolerance])
+ballColor = ([153,152,97], [33*tolerance,38*tolerance,20*tolerance])
 
-    with HandClient() as hand, arm.connection():
-        try:
-            initArm(arm)
-            moveHand(hand, handOpen)
-
-            a = getBrightness(arm)
-            print(a)
-
-            moveHand(hand, handClosed)
-        finally:
-            arm.movement.brakes.brake().ok()
-
-def getCameraFrame(arm, cameraRequest): 
-    res = arm.camera.data.get_color_frame(cameraRequest)
-    raw_data = res.response.data
-    base64_data = raw_data.decode().split(",")[1]
-    image_data = base64.b64decode(base64_data)
-    np_byte_array = np.frombuffer(image_data, dtype=np.uint8)
-    pixel_array_bgr = cv2.imdecode(np_byte_array, cv2.IMREAD_COLOR)
-    pixel_array_rgb = cv2.cvtColor(pixel_array_bgr, cv2.COLOR_BGR2RGB)
-    return pixel_array_rgb
-
-def getBrightness(arm, cameraRequest = models.CameraFrameRequest(
+defaultCameraRequest = models.CameraFrameRequest(
             camera_settings=models.CameraSettings(
                 brightness=0,
                 contrast=50,
@@ -59,25 +35,58 @@ def getBrightness(arm, cameraRequest = models.CameraFrameRequest(
                 autoWhiteBalance=True,
             )
         )
+
+def main():
+    with ArmClient() as arm:
+        # initArm(arm)
+        # moveHand(hand, handOpen)
+
+        # print(getBrightness(arm))
+
+        frame = getCameraFrame(arm)
+        # print(frame.shape)
+        # print(frame[360,640])
+
+        # isBall = np.vectorize(lambda pix: (abs(pix[0] - ballColor[0][0]) < ballColor[1][0]) and (abs(pix[1] - ballColor[0][1]) < ballColor[1][1]) and (abs(pix[2] - ballColor[0][2]) < ballColor[1][2]))
+        # isBallFrame = isBall(pix=(frame[:,:,0],frame[:,:,1],frame[:,:,2]))
+        frame0 = frame[:,:,0]
+        isBall0 = abs(frame0 - ballColor[0][0]) < ballColor[1][0]
+        frame1 = frame[:,:,1]
+        isBall1 = abs(frame1 - ballColor[0][1]) < ballColor[1][1]
+        frame2 = frame[:,:,2]
+        isBall2 = abs(frame2 - ballColor[0][2]) < ballColor[1][2]
+
+        isBallFrame = isBall2 & isBall1 & isBall0
+
+        out = np.where(isBallFrame[...,None], np.array([np.uint8(0),np.uint8(0),np.uint8(255)]), np.array([np.uint8(0),np.uint8(0),np.uint8(0)]))
+
+        cv2.imwrite("frame.jpeg", frame)
+        cv2.imwrite("filtered.jpeg", out)
+
+        # moveHand(hand, handClosed)
+
+def getCameraFrame(arm, cameraRequest = defaultCameraRequest): 
+    res = arm.camera.data.get_color_frame(cameraRequest)
+    raw_data = res.response.data
+    base64_data = raw_data.decode().split(",")[1]
+    image_data = base64.b64decode(base64_data)
+    np_byte_array = np.frombuffer(image_data, dtype=np.uint8)
+    pixel_array_bgr = cv2.imdecode(np_byte_array, cv2.IMREAD_COLOR)
+    pixel_array_rgb = cv2.cvtColor(pixel_array_bgr, cv2.COLOR_BGR2RGB)
+    return pixel_array_rgb
+
+def getBrightness(arm, cameraRequest = defaultCameraRequest
     ): 
     pixel_array_rgb = getCameraFrame(arm, cameraRequest)
     flat = pixel_array_rgb.flatten()
     avg = np.mean(flat)
     return avg
 
-def getTennisBallLoc3D(arm, cameraRequest = models.CameraFrameRequest(
-            camera_settings=models.CameraSettings(
-                brightness=0,
-                contrast=50,
-                exposure=350,
-                sharpness=50,
-                hue=0,
-                whiteBalance=4600,
-                autoWhiteBalance=True,
-            )), 
-        tennisBallAcceptColors = ([1,2,3],[2,3,4])
+def getTennisBallLoc3D(arm, cameraRequest = defaultCameraRequest, 
+        tennisBallAcceptColors = ballColor
     ): 
     pixel_array_rgb = getCameraFrame(arm, cameraRequest)
+    # pixel_array_tennis_ball = 
     pixel_array_tennis_ball = pixel_array_rgb > tennisBallAcceptColors[0] and pixel_array_rgb < tennisBallAcceptColors[1] #TODO: FIX SYNTAX
     tennisBallLoc2D = fitCircleInArray(pixel_array_tennis_ball)
     tennisBallOffset = 0 #TODO: Implement
@@ -116,6 +125,19 @@ class HandClient:
 
     def __exit__(self,a,b,c):
         self.hand.close()
+
+class ArmClient:
+    def __enter__(self):
+        self.sdk = StandardBotsRobot(
+            url='http://192.168.1.3:3000', 
+            token=Path("/home/max/Documents/.robot_token").read_text().strip(),
+            robot_kind=StandardBotsRobot.RobotKind.Live,
+        )
+        return self.sdk
+
+    def __exit__(self, a, b, c):
+        self.sdk.movement.brakes.brake().ok()
+        self.sdk._request_manager.close()
 
 if __name__ == "__main__":
     main()
